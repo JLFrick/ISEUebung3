a54239e49da0c1daed7aff67ee9661f2aa9aab9c
Jan-Luca Frick <jan-lucafrick@10.102.200.40>
2018-11-16 16:04:09 +0100

    RechnerTest Change

--- test/rechner/RechnerTest.java
+++ test/rechner/RechnerTest.java
@@ -1,23 +1,26 @@
 package rechner;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class RechnerTest {
+
 	@Test
-	void testadd() {
+	public void testAddNumb() {
 		Rechner rechnen = new Rechner();
 		assertEquals(2,rechnen.addNumb(1.5,0.5));
 	}
+
 	@Test
-	void testmult() {
-		Rechner rechnen = new Rechner();
-		assertEquals(1,rechnen.mulNumb(2,0.5));
-	}
-	@Test
-	void testsub() {
+	public void testSubNumb() {
 		Rechner rechnen = new Rechner();
 		assertEquals(1.5,rechnen.subNumb(2,0.5));
+	}
+
+	@Test
+	public void testMulNumb() {
+		Rechner rechnen = new Rechner();
+		assertEquals(1,rechnen.mulNumb(2,0.5));
 	}
 }
